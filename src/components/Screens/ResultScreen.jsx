19e5cde47380a510307e51443821ee224a63167c
JiaXiang114462003 <114462003@g.nccu.edu.tw>
2025-12-15 13:57:28 +0800

    delete unnecessary file

--- src/components/Screens/ResultScreen.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-/**
- * 結算畫面元件
- * 用於顯示第一階段結束以及遊戲結束結果 (包含頭銜判斷)
- */
-export default function ResultScreen({ score, title, onRestart }) {
-	return <div className="result-screen">{/* 結算畫面內容將在此實現 */}</div>;
-}
